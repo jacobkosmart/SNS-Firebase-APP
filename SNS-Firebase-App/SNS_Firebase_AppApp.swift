8d9a112d0086bd2ba13264bcc598492e83d67bd7
jacobkosmart <jacobkosmart@gmail.com>
2023-02-04 14:27:53 +0900

    Initial Commit

---
+++ SNS-Firebase-App/SNS_Firebase_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNS_Firebase_AppApp.swift
+//  SNS-Firebase-App
+//
+//  Created by Jacob Ko on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct SNS_Firebase_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
